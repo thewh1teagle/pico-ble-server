0c7a6205d1400805fd7e57d334285a179ab88303
user <61390950+thewh1teagle@users.noreply.github.com>
2023-12-30 00:25:38 +0200

    add python example

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import asyncio
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient, BleakGATTServiceCollection
+import struct
 
 UUID = '0000181a-0000-1000-8000-00805f9b34fb'
 
-async def main():
+async def find_address():
     devices = await BleakScanner.discover()
     for d in devices:
-        print(d.details)
+        details = d.details
+        if UUID in details['props']['UUIDs']:
+            return d
+
+async def find_char(services: BleakGATTServiceCollection):  
+    for id, handle in services.characteristics.items():
+        desc = handle.description
+        if desc == 'Temperature':
+            return handle
+        
+async def main():
+    device = await find_address()
+    async with BleakClient(device) as client:
+        services = client.services
+        char = await find_char(services)
+        while True:
+            packet = await client.read_gatt_char(char)
+            temp, = struct.unpack('<H', packet) # uint16_t
+            # Convert the uint16_t to a float
+            temp = temp / 100.0
+            print(f'Temp is {temp}')
+            await asyncio.sleep(1)
 
 asyncio.run(main())
